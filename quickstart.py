c5a4e2e7fc3374737eb30adfa07d06a5dd8abeec
frno0254 <smallgreycreatures@gmail.com>
2015-04-19 10:59:34 +0200

    iterates over folder and downloads file

--- quickstart.py
+++ quickstart.py
@@ -6,5 +6,28 @@ gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentica
 drive = GoogleDrive(gauth)
 
 file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+exists = False
+
+for file1 in file_list:
+	print 'title: %s, id %s' % (file1['title'], file1['id'])
+
+	if file1['title'] == 'Hello.txt':
+		exists = True
+
+	elif file1['title'] == 'RapportMall':
+		print 'found %s' %file1['title']
+		content = file1.GetContentFile('test.html', mimetype='text/html')
+
+if exists == True:
+	print 'file already exists'
+
+else:
+	file1 = drive.CreateFile({'title': 'Hello.txt'})
+	file1.Upload()
+	print 'title: %s, id %s' % (file1['title'], file1['id'])
+
+#Iterate over files
+file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
 for file1 in file_list:
   print 'title: %s, id: %s' % (file1['title'], file1['id'])
+
